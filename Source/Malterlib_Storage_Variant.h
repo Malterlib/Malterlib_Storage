eb76073ac0a9dfffa77369de6de19a62cb1a3a86
Erik Olofsson <erik@olofsson.info>
2024-06-01 19:08:47 +0200

    [Malterlib/Storage] Fix support for using scoped enums as index type in TCStreamableVariant

--- Source/Malterlib_Storage_Variant.h
+++ Source/Malterlib_Storage_Variant.h
@@ -382,7 +382,7 @@ namespace NMib::NStorage
 	template <typename t_CIndex, typename ...tp_CTypes, t_CIndex ...tp_Member>
 	struct TCVariantCommon<t_CIndex, TCVariantMember<t_CIndex, tp_CTypes, tp_Member>...>
 #ifdef DMibDebuggerHelpers
-		: public TCVariantCommonDebugHelperMember<tp_Member, tp_CTypes>...
+		: public TCVariantCommonDebugHelperMember<smint(tp_Member), tp_CTypes>...
 #endif
 	{
 		using CIndexType = t_CIndex;
